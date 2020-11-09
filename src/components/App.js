c4324f8ae19efa0855a826d4234152bf27f76b4d
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-10 01:53:01 +0530

    code-updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,20 +2,21 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [change, setChange] = useState(true);
   return (
     <div id="main">
-      <button
-        id="click"
-        onClick={() => (
-          <p id="para">
-            Hello, I've learnt to use the full-stack evaluation tool. This makes
-            me so happy
-          </p>
-        )}
-      >
+      <button id="click" onClick={() => setChange(!change)}>
         {" "}
         click Me{" "}
       </button>
+      {change ? (
+        <p></p>
+      ) : (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      )}
     </div>
   );
 }
