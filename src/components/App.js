9742cb20ce59add6e54704a9917ea4e2021932c0
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-10 01:25:43 +0530

    code-updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,20 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  const [change, setChange] = useState(true);
+  function rnder() {
+    return (
+      <p id="para">
+        Hello, I've learnt to use the full-stack evaluation tool. This makes me
+        so happy
+      </p>
+    );
+  }
   return (
     <div id="main">
-      <button id="click" onClick={() => setChange(!change)}>
+      <button id="click" onClick={rnder()}>
         {" "}
         click Me{" "}
       </button>
-      {change ? (
-        <p></p>
-      ) : (
-        <p id="para">
-          Hello, I've learnt to use the full-stack evaluation tool. This makes
-          me so happy
-        </p>
-      )}
     </div>
   );
 }
