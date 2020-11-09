88f84dc907fd3424cb4bf37c45b968f3e2518d7f
Madhav Milan <49267441+m2-madhav@users.noreply.github.com>
2020-11-10 01:33:50 +0530

    code -updated

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,17 @@ import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
-  function rnder() {
-    return (
-      <p id="para">
-        Hello, I've learnt to use the full-stack evaluation tool. This makes me
-        so happy
-      </p>
-    );
-  }
   return (
     <div id="main">
-      <button id="click" onClick={rnder()}>
+      <button
+        id="click"
+        onClick={() => (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        )}
+      >
         {" "}
         click Me{" "}
       </button>
